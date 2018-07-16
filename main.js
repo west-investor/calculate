679f3e8719df4972cd7ca96626c5959de4acac59
west-investor <vlad.rud.2000@gmail.com>
2018-07-16 11:27:42 +0300

    all

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+
+function fun1() {
+	var firstNumber = document.getElementById('firstNumb').value;
+	var secondNumber = document.getElementById('secondNumb').value;
+	var result = document.getElementById('newNumb');
+	var select = document.getElementById('selectOption').selectedIndex;
+
+	if (select == '0') {
+		result.innerHTML = +firstNumber + +secondNumber;
+	} else if (select == '1') {
+		result.innerHTML = firstNumber - secondNumber;
+	} else if (select == '2') {
+		result.innerHTML = firstNumber * secondNumber;
+	} else if (select == '3') {
+		result.innerHTML = firstNumber / secondNumber;
+	} else {
+		alert("hell");
+	}
+
+}
